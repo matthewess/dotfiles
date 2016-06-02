3bf17f73723ca4dac61554eb0bd0fb16a3b4449f
Jay Hankins <jay.hankins@outlook.com>
2016-06-02 14:32:44 -0400

    update

--- prezto.sh
+++ prezto.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env zsh
 git clone --recursive https://github.com/jay-hankins/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 chsh -s /bin/zsh
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
