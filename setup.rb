df812936cd5a199e0ad294d62086d1b51b722551
Jay Hankins <jay.hankins@outlook.com>
2016-06-03 14:11:32 -0400

    exec

--- setup.rb
+++ setup.rb
@@ -131,7 +131,7 @@ class Dot
   end
 
   def brew
-    `/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"`
+    exec '/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"'
   end
 
   def bundle
